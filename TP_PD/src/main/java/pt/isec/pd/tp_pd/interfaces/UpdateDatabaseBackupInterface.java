7388838f1c50504cee6076ddf249813a7a6bfc6d
diogocsgomes <54036658+diogocsgomes@users.noreply.github.com>
2024-01-26 01:32:14 +0000

    Delete TP_PD directory

--- TP_PD/src/main/java/pt/isec/pd/tp_pd/interfaces/UpdateDatabaseBackupInterface.java
+++
@@ -1,8 +0,0 @@
-package pt.isec.pd.tp_pd.interfaces;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface UpdateDatabaseBackupInterface extends Remote {
-    void update() throws RemoteException;
-}
